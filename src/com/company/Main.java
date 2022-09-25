10d57a52759621d5ffe8386981f1f1c418596858
Nijat Gahramanov <nejatqahramanov7@gmail.com>
2022-09-25 23:21:35 +0300

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator operation = new Calculator(5.0,6.0);
-        double result = operation.calculate(new Subtraction());
+        double result = operation.calculate(new Addition());
 
         System.out.println(result);
 
